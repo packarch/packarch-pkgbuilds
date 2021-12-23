57b0109244077c6f720ecf87281aa1212e9ae906
Antidote1911 <antidote1911@gmail.com>
2021-12-23 03:51:16 +0100

    up

--- packarch-xfce-config/files/xmenu/xmenu_screenshots.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-xmenu <<EOF | sh &
-Ecran entier							xfce4-screenshooter -f
-Ecran entier 5s							xfce4-screenshooter -f -d 5
-Ecran entier 10s						xfce4-screenshooter -f -d 10
-Région séléctionnée						xfce4-screenshooter -r
-Fenêtre séléctionnée dans 5s			xfce4-screenshooter -w -d 5
-EOF
