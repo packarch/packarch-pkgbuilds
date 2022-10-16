3d375e432bcbd46a1550fa63891c7e01409b1e3a
Antidote1911 <antidote1911@gmail.com>
2022-10-16 21:02:31 +0200

    up

--- packarch-skel/files/skel_dir/.config/openbox/polybar/hack/scripts/updates.sh
+++ packarch-skel/files/skel_dir/.config/openbox/polybar/hack/scripts/updates.sh
@@ -5,7 +5,7 @@
 
 NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
 
-get_total_updates() { UPDATES=$(checkupdates 2>/dev/null | wc -l); }
+get_total_updates() { UPDATES=$(checkupdates.sh 2>/dev/null | wc -l); }
 
 while true; do
     get_total_updates
